7e88fd42d3e629e9c4492ff33ca2239f2fc6632e
Oleksandr-Berd <alex.berd86@gmail.com>
2023-12-16 18:50:59 +0200

    code finished

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const connectDB = async () => {
-    try {
-        mongoose.set('strictQuery', false)
-        const conn = await mongoose.connect(process.env.DB_URI)
+  
+  try {
+    mongoose.set('strictQuery', false);
+    const conn = await mongoose.connect(process.env.DB_URI);
+    console.log(`Database Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
 
-        console.log(`Database connectet ${conn.connection.host}`);
-    } catch (error) {
-        console.log(error);
-    }
 }
 
-module.exports = connectDB
+module.exports = connectDB;
